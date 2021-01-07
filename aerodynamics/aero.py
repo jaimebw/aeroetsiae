ec96d9a0baf5adb4162af318cadb8ff5e6589bd7
jaimebw <jaimebwv@gmail.com>
2021-01-07 18:32:55 +0100

    Added support for NACA profile

--- aerodynamics/aero.py
+++ aerodynamics/aero.py
@@ -6,11 +6,22 @@ import os
 import matplotlib.pyplot as plt 
 
 class airfoil:
-    def __init__(self,DNI,number_coefs,rudder= None):
+    def __init__(self,NACA = None,delta_a = None,ca = None,DNI= None,number_coefs = 100,rudder= None):
         self.theta = np.linspace(0,pi,100) 
         self.dni = DNI
-        self.n = number_coefs
-        self.timon = rudder
+        self.n = np.linspace(0,number_coefs,number_coefs+1)
+        self.rudder = rudder
+        self.NACA = NACA
+        if NACA != None:
+            try:
+                self.f = int(NACA[0])/100
+                self.xf = int(NACA[1])/10
+                self.t = int(NACA[2:3])/100
+                self.ca = ca
+                self.delta_a = delta_a
+            except ValueError:
+                print("Perfil NACA no valido, revisa que las cifras son correctas, no pongas la letra del final")
+        
         if self.rudder == None:
             self.T = 0
         elif self.rudder == 0:
@@ -19,15 +30,16 @@ class airfoil:
             self.T = 2
         if not os.path.exists("airfoil_data"):
             os.mkdir("airfoil_data")
-        try:
-            self.f = 1 + int(int(self.dni[3])*5/8)/100
-            self.xf = 20 + 10*int(int(self.dni[4])/8*2)/100
-            self.t = 8 +int(int(self.dni[5])*10/9)/100
-            self.ca = 20 +5*int(int(self.dni[6])*3/8)/100
-            self.delta_a = np.radians(-(5 + 5*int(int(self.dni[7])*2/8)))
-            #self.parametros = np.array([self.f,self.xf,self.t,self.ca,self.delta_a])/100
-        except ValueError:
-            print("DNI no valido, revisa que las cifras son correctas, no pongas la letra del final")
+        if NACA == None:
+            try:
+                self.f = 1 + int(int(self.dni[3])*5/8)/100
+                self.xf = 20 + 10*int(int(self.dni[4])/8*2)/100
+                self.t = 8 +int(int(self.dni[5])*10/9)/100
+                self.ca = 20 +5*int(int(self.dni[6])*3/8)/100
+                self.delta_a = np.radians(-(5 + 5*int(int(self.dni[7])*2/8)))
+                #self.parametros = np.array([self.f,self.xf,self.t,self.ca,self.delta_a])/100
+            except ValueError:
+                print("DNI no valido, revisa que las cifras son correctas, no pongas la letra del final")
         
 
 
@@ -85,12 +97,13 @@ class airfoil:
         self.coefs = coefs
         np.savetxt("coefs.csv", coefs, delimiter=",")
         return coefs
-    @coeficientes
+   
     def lift_coef(self):
         """
         Returns the cl and cp of the airfoil
         """
-        alpha_ST = - self.a0[self.T]
+        coefs = self.coeficientes()
+        alpha_ST = - coef[0]
         x = 0.5+0.5*cos(self.theta)
         def sumAN(theta,coefn,n):
             Sumatorio = np.array([])
@@ -109,17 +122,16 @@ class airfoil:
         self.cp,self.cl = cp,cl
         return cl, cp
     
-    @coeficientes
-    @lift_coef
+    """
     def plot_cp_cl(self):
-        """
-        Plots de cp and cl over the profile
-        """
+        
+        #Plots de cp and cl over the profile
+        
         fig , ax = plt.subplots()
         ax.plot(self.x,self.cp)
         ax.set_ylabel("Chord")
         ax.set_xlabel("Cp/cl")
-
+    """
 
 
 
